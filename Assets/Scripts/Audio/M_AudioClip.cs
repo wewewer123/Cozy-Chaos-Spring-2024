82d04dd0b7bef971a24ecf26c23ca032a7ad9a84
Rakshat Kashyap <kashyaprakshat@gmail.com>
2024-04-19 22:27:33 +0530

    Created AudioManager

---
+++ Assets/Scripts/Audio/M_AudioClip.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace CrazyChaosSpring2024
+{
+    public enum ClipType
+    {
+        Menu,
+        Level
+    }
+    [Serializable]
+    public class M_AudioClip
+    {
+        public string name;
+        public AudioClip clip;
+        public ClipType type;
+    }
+}
