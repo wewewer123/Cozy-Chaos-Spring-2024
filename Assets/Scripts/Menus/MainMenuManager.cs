17a7f994a0903c02bd9afa622b7079ddfce7e34c
Rakshat Kashyap <kashyaprakshat@gmail.com>
2024-04-21 14:27:06 +0530

    Added new UI animations to Main Menu

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -2,14 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using CozyChaosSpring2024;
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 namespace CrazyChaosSpring2024
 {
     public class MainMenuManager : MonoBehaviour
     {
+        
+        [SerializeField] private RectTransform optionsPanel; 
         private UIAnimation[] _uiAnimationsElements;
+        public const float UIElemAnimDuration = 0.6f;
+
+        private const float OptionsPanelAnimDuration = 0.6f;
 
         private void Awake()
         {
@@ -27,11 +34,28 @@ namespace CrazyChaosSpring2024
             {
                 uiElem.Shrink();
             }
+
+            Invoke(nameof(SlideIn), UIElemAnimDuration);
+            return;
         }
+
+        private void SlideIn() => optionsPanel.DOAnchorPos(new Vector2(960, 0), OptionsPanelAnimDuration);
 
         public void OnCreditsClicked()
         {
             
         }
+
+        public void OnOptionsMenuClosed()
+        {
+            foreach (var uiElem in _uiAnimationsElements)
+            {
+                uiElem.Grow();
+            }
+
+            Invoke(nameof(SlideOut), UIElemAnimDuration);
+        }
+
+        private void SlideOut() => optionsPanel.DOAnchorPos(new Vector2(-1000, 0), OptionsPanelAnimDuration);
     }
 }
--- Assets/Scripts/Menus/UIAnimation.cs
+++ Assets/Scripts/Menus/UIAnimation.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using CrazyChaosSpring2024;
 using DG.Tweening;
 using UnityEngine;
 
@@ -8,17 +9,24 @@ namespace CozyChaosSpring2024
 {
     public class UIAnimation : MonoBehaviour
     {
-        private static float TweeningDuration = 0.6f;
         private RectTransform _rectTransform;
+
+        private Vector2 _defaultSizeDelta;
 
         private void Awake()
         {
             _rectTransform = GetComponent<RectTransform>();
+            _defaultSizeDelta = _rectTransform.sizeDelta;
         }
 
         public void Shrink()
         {
-            _rectTransform.DOSizeDelta(new Vector2(0, 0), TweeningDuration).SetEase(Ease.InElastic);
+            // var scale = 1;
+            // DOTween.To(() => scale, x => scale = x, 0, MainMenuManager.UIElemAnimDuration)
+                // .OnUpdate(() => _rectTransform.localScale = new Vector3(scale, scale, 0));
+            _rectTransform.DOSizeDelta(new Vector2(0, 0), MainMenuManager.UIElemAnimDuration).SetEase(Ease.InElastic);
         }
+
+        public void Grow() => _rectTransform.DOSizeDelta(_defaultSizeDelta, MainMenuManager.UIElemAnimDuration).SetEase(Ease.OutElastic);
     }
 }
