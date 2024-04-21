8f92239222ca5570e36671139dca11285b35ff92
HéctorTL <78750635+HTLGames@users.noreply.github.com>
2024-04-21 20:45:38 +0200

    Quick menu fixes

--- Assets/Scripts/Menus/UIAnimation.cs
+++ Assets/Scripts/Menus/UIAnimation.cs
@@ -19,8 +19,8 @@ namespace CozyChaosSpring2024
             _defaultSizeDelta = _rectTransform.sizeDelta;
         }
 
-        public void Shrink() => _rectTransform.DOSizeDelta(new Vector2(0, 0), MainMenuManager.UIElemAnimDuration).SetEase(Ease.InElastic);
+        public void Shrink() => _rectTransform.DOSizeDelta(new Vector2(0, 0), MainMenuManager.UIElemAnimDuration).SetEase(Ease.InCubic);
 
-        public void Grow() => _rectTransform.DOSizeDelta(_defaultSizeDelta, MainMenuManager.UIElemAnimDuration).SetEase(Ease.OutElastic);
+        public void Grow() => _rectTransform.DOSizeDelta(_defaultSizeDelta, MainMenuManager.UIElemAnimDuration).SetEase(Ease.OutCubic);
     }
 }
